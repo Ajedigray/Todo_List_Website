2d7e925e29fafa81fdb8ee8068a1f1ba1f1d9890
AJ Gray <awgray@AJs-MacBook-Pro.fios-router.home>
2017-11-18 23:18:16 -0500

    Refactor code

--- js/list.js
+++ js/list.js
@@ -4,21 +4,33 @@ var todos = [];
 while (input != "quit"){
 	var input = prompt("What would you like to do?");
 	if (input === "list"){
-		console.log(todos);
-		todos.forEach(function(todo, index){
-			console.log("*********");
-			console.log(index + ": " +todo);
-			console.log("*********");
-		});
+		listTodos();
 	}
 	else if (input === "new") {
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+		addTodo();
 	}
 	else if (input === "delete"){
-		var index_to_delete = prompt("What item would you like to delete?");
-		todos.splice(index_to_delete, 1);
+		deleteTodo();
+	}
+	else{
+		console.log("OK YOU QUIT THE APP");
 	}
 }
-console.log("OK YOU QUIT THE APP");
-	
+function listTodos(){
+	console.log(todos);
+	todos.forEach(function(todo, index){
+		console.log("*********");
+		console.log(index + ": " +todo);
+		console.log("*********");
+	});
+}
+
+function addTodo(){
+	var newTodo = prompt("Enter new todo");
+	todos.push(newTodo);
+}
+
+function deleteTodo(){
+	var index_to_delete = prompt("What item would you like to delete?");
+	todos.splice(index_to_delete, 1);	
+}
